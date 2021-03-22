4f0487a2b04aa166a479c89efbacdddeffdc6692
aleksandern NA <aleksandern08@gmail.com>
2021-03-23 01:30:39 +0700

    hz

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
